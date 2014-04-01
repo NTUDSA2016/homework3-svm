64d3f7dec6300f48045cb08e28a09edb3755b5b3
suhorng <suhorng@gmail.com>
2014-04-02 02:59:37 +0800

    implement tokenizer interface

--- cpp/include/tokenizer.h
+++ cpp/include/tokenizer.h
@@ -5,9 +5,40 @@
 
 class Tokenizer {
   std::istream& input;
+  std::string next_token;
+  std::string token_buf;
+  std::string::const_iterator cur, end;
+  bool is_eof;
+
+  void fill_token();
 public:
+  /*
+  @param input_stream: the input of the tokenizer
+  @description:
+    create a tokenizer that takes the input from
+    `input_stream`. `input_stream` should be alive
+    through the lifetime of the Tokenizer.
+  */
   Tokenizer(std::istream& input_stream)
-    : input(input_stream) {}
+    : input(input_stream), is_eof(false)
+  {
+    this->cur = token_buf.end();
+    this->end = token_buf.end();
+  }
+
+  /*
+  @description: return if there are any tokens
+  */
+  bool has_more();
+
+  /*
+  @description: peek the next token in the buffer
+  */
   std::string peekToken();
+
+  /*
+  @description:
+    remove the next token from the buffer and return it
+  */
   std::string nextToken();
 };
--- cpp/src/tokenizer.cpp
+++ cpp/src/tokenizer.cpp
@@ -1,11 +1,42 @@
 #include <fstream>
 #include <string>
+#include <stdexcept>
 #include "tokenizer.h"
 
 using std::string;
 
+bool Tokenizer::has_more() {
+  this->fill_token();
+  return not this->is_eof;
+}
+
 string Tokenizer::peekToken() {
+  this->fill_token();
+  if (this->is_eof)
+    throw std::runtime_error("Tokenizer::peekToken(): going over end of input");
+  return this->next_token;
 }
 
 string Tokenizer::nextToken() {
+  this->fill_token();
+  if (this->is_eof)
+    throw std::runtime_error("Tokenizer::nextToken(): going over end of input");
+  string token;
+  this->next_token.swap(token);
+  return token;
+}
+
+void Tokenizer::fill_token() {
+  if (this->is_eof or not this->next_token.empty())
+    return;
+  for (;;) {
+    if (this->cur == this->end) {
+      if (not std::getline(this->input, this->token_buf)) {
+        this->is_eof = true;
+        break;
+      }
+      this->cur = this->token_buf.begin();
+      this->end = this->token_buf.end();
+    }
+  }
 }
