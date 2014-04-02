d4fc805da77924caaa605875b771948e0657656d
suhorng <suhorng@gmail.com>
2014-04-03 00:03:34 +0800

    fix tokenizer empty line bug

--- cpp/src/tokenizer.cpp
+++ cpp/src/tokenizer.cpp
@@ -26,7 +26,7 @@ void Tokenizer::fillToken() {
   using std::isalnum;
   using std::isspace;
   static const char* syms[] = {
-    "->", "<=", "\\", "(", ")",
+    "->", "<=", "\\", "(", ")", "=" /* extension */,
     "@", "^", "*", "/", "+", "-",
     nullptr
   };
@@ -42,16 +42,18 @@ void Tokenizer::fillToken() {
       this->end = this->token_buf.cend();
     }
     string::const_iterator nxt = this->cur;
-    if (isspace(*nxt)) {
+    if (this->token_buf.empty()) {
+      continue;
+    } else if (isspace(*nxt)) {
       ++this->cur;
       continue;
     } else if (nxt+1 != this->end and *nxt=='/' and *(nxt+1)=='/') { // comment
       this->cur = this->end;
       continue;
     } else if (isdigit(*nxt)) { // [0-9]+
-      while (isdigit(*nxt)) ++nxt;
+      while (nxt!=this->end and isdigit(*nxt)) ++nxt;
     } else if (isalpha(*this->cur) or *this->cur=='_') { // [_a-zA-z][_a-zA-z0-9]*
-      while (isalnum(*nxt) or *nxt=='_') ++nxt;
+      while (nxt!=this->end and (isalnum(*nxt) or *nxt=='_')) ++nxt;
     } else { // symbols
       size_t i;
       for (i = 0; syms[i]; ++i) {
